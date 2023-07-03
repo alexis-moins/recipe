f743ce04494cc98cdfef2201373912ccea4a2763
alexis-moins <sinus.nacre0v@icloud.com>
2023-07-03 14:08:36 +0200

    refactor: updated clone command with new confirm function

--- src/clone_command.sh
+++ src/clone_command.sh
@@ -1,7 +1,7 @@
 local url="${args[url]}"
 local auto_confirm="${args[--yes]}"
 
-if [[ -n "${auto_confirm}" ]] || "${deps[gum]}" confirm "Overwrite current recipe book?"; then
+if [[ -n "${auto_confirm}" ]] || confirm "Overwrite current recipe book?"; then
     run_silent rm -rf "${RECIPE_BOOK_DIR}"
     git clone "${url}" "${RECIPE_BOOK_DIR}"
 
