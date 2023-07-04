25ce56f7710788d80d1c4d3fd989032733222b4c
alexis-moins <sinus.nacre0v@icloud.com>
2023-07-04 19:26:54 +0200

    feat: added --templates option to list command

--- src/list_command.sh
+++ src/list_command.sh
@@ -1,1 +1,7 @@
-find_recipe
+local templates="${args[--templates]}"
+
+if [[ -z "${templates}" ]]; then
+    find_recipe
+elif [[ -f "${RECIPE_BOOK_DIR}/.templates" ]]; then
+    \rg -N --color=never --only-matching '([\w/]+):' --replace '$1' "${RECIPE_BOOK_DIR}/.templates"
+fi
