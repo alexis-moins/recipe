6dc78056da933ac3b933e7a15969e2ba33df3713
alexis-moins <sinus.nacre0v@icloud.com>
2023-07-06 16:09:24 +0200

    feat: updated confirm function

--- src/lib/confirm.sh
+++ src/lib/confirm.sh
@@ -1,4 +1,4 @@
 confirm() {
-    local response=`gum input --prompt "${1} $(green "(yes/no [no])") " --placeholder ""`
+    local response=`gum input --prompt "${1} $(green "(yes|no)") " --placeholder ""`
     [[ "${response}" == "yes" ]] && return 0 || return 1
 }
