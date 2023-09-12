16aafb762b3cb15972b2c6eb13bc5379ef80c96b
alexis-moins <sinus.nacre0v@icloud.com>
2023-09-12 15:34:55 +0200

    feat: removed templating + added edit option to use command

--- src/use_command.sh
+++ src/use_command.sh
@@ -1,32 +1,35 @@
 local recipe="${args[name]}"
 local destination="${args[--destination]}"
+local edit="${args[--edit]}"
 
 [[ -z "${recipe}" ]] && recipe="$(filter_recipe)"
 
 [[ -z "${destination}" ]] && destination="$(basename ${recipe})"
 
 if [[ ! -f "${destination}" ]] || confirm "Overwrite ${destination}?"; then
-    local variables=`rg -N "${recipe}:" "${RECIPE_BOOK_DIR}/.templates"`
+    # local variables=`rg -N "${recipe}:" "${RECIPE_BOOK_DIR}/.templates"`
 
-    if [[ -z "${variables}" ]]; then
-        \cp -f "${RECIPE_BOOK_DIR}/${recipe}" "${destination}"
-    else
-        local expression=""
+    # if [[ -z "${variables}" ]]; then
+    \cp -f "${RECIPE_BOOK_DIR}/${recipe}" "${destination}"
+    # else
+    #     local expression=""
 
-        for template in ${variables/${recipe}: /}; do
-            [[ -n "${expression}" ]] && expression+=";"
-            local response=`gum input --prompt "${template}: " --placeholder "something"`
+    #     for template in ${variables/${recipe}: /}; do
+    #         [[ -n "${expression}" ]] && expression+=";"
+    #         local response=`gum input --prompt "${template}: " --placeholder "something"`
 
-            [[ -z "${response}" ]] && exit 1
+    #         [[ -z "${response}" ]] && exit 1
 
-            echo "${template}: $(cyan ${response})"
+    #         echo "${template}: $(cyan ${response})"
 
-            expression+="s/{{ ${template} }}/${response}/"
-        done
+    #         expression+="s/{{ ${template} }}/${response}/"
+    #     done
 
-        sed "${expression}" "${RECIPE_BOOK_DIR}/${recipe}" > "${destination}"
-        echo ""
-    fi
+    #     sed "${expression}" "${RECIPE_BOOK_DIR}/${recipe}" > "${destination}"
+    #     echo ""
+    # fi
 
     echo "$(green ✔) Your recipe is ready to use"
+
+    [[ -n "${edit}" ]] && $EDITOR "${destination}"
 fi
